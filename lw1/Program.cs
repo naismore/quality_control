2b37fcdbbdf9600409c104b0e89eac248746ab91
naismore <anxismore@gmail.com>
2025-03-25 12:18:14 +0300

    lw1

---
+++ lw1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lw1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            System.Console.WriteLine("Unhandled exception");
+            Environment.Exit(1);
+        }
+
+        
+    }
+}
